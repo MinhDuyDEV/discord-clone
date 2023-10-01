7377ca6a58cb2c0917c537e6d9ef3c55bbb26357
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-01 15:59:21 +0700

    Prisma & postgresql setup docker & init modal UI

---
+++ src/app/(setup)/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import InitialModal from "@/components/modals/initial-modal";
+
+const page = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) {
+    redirect(`/server/${server.id}`);
+  }
+  return <InitialModal />;
+};
+
+export default page;
