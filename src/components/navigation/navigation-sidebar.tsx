1792164a952e3455561377428139813330d1ddd1
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-02 11:20:43 +0700

    Navigation Sidebar

--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,56 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ModeToggle } from "@/components/mode-toggle";
+import { redirect } from "next/navigation";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import NavigationAction from "@/components/navigation/navigation-action";
+import NavigationItem from "@/components/navigation/navigation-item";
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
-const NavigationSidebar = () => {
-  return <></>;
+const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirect("/");
+  }
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+      <NavigationAction />
+      <Separator className="h-0.5 bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="flex flex-col items-center pb-3 mt-auto gap-y-4">
+        <ModeToggle></ModeToggle>
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-12 w-12",
+            },
+          }}
+        ></UserButton>
+      </div>
+    </div>
+  );
 };
 
 export default NavigationSidebar;
