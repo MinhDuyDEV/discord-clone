e8d156f10d4295142501e322cf76d7f5b1295e00
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-02 13:52:10 +0700

    Create server modal

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 
-const layout = async ({ children }: { children: React.ReactNode }) => {
+const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full ">
-      <div className="fixed inset-y-0 z-30 flex-col hidden h-full md:flex w-[72px]">
+    <div className="h-full">
+      <div className="flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
         <NavigationSidebar />
       </div>
       <main className="md:pl-[72px] h-full">{children}</main>
@@ -12,4 +11,4 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default layout;
+export default MainLayout;
