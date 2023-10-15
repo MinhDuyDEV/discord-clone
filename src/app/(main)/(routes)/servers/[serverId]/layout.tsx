da7c82afb90566acfe024581acf12d2d4947d709
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-15 10:47:50 +0700

    Add ChatHeader

--- src/app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -31,7 +31,7 @@ const layout = async ({
   }
   return (
     <div className="h-full">
-      <div className="fixed inset-y-0 z-20 flex-col h-full md:flex w-60">
+      <div className="fixed inset-y-0 z-20 flex-col hidden h-full md:!flex w-60">
         <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -36,7 +36,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       <DropdownMenuTrigger className="focus:outline-none" asChild>
         <button className="flex items-center w-full h-12 px-3 font-semibold transition border-b-2 outline-none text-md border-neutral-200 dark:border-neutral-800 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50">
           {server.name}
-          <ChevronDown className="w-5 h-5 ml-auto" />
+          <ChevronDown className="relative w-5 h-5 ml-auto mr-8 md:ml-auto md:mr-0" />
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 space-y-0.5 text-xs font-medium text-black dark:text-neutral-400 ">
