880082568e51a0824c8b7cd7105f374308a375c9
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-15 09:53:33 +0700

    Add Channel ID page

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>Server Id page</div>;
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const page = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+  const initialChannel = server?.channels[0];
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+  return redirect(`/servers/${server?.id}/channels/${initialChannel.id}`);
 };
 
 export default page;
--- src/components/server/server-channel.tsx
+++ src/components/server/server-channel.tsx
@@ -6,7 +6,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
 import ActionTooltip from "../action-tooltip";
-import { useModal } from "@/hooks/use-modal-store";
+import { ModalType, useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -21,13 +21,21 @@ const iconMap = {
 };
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
+  const { onOpen } = useModal();
   const params = useParams();
   const router = useRouter();
   const Icon = iconMap[channel.type];
-  const { onOpen } = useModal();
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { server, channel });
+  };
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -47,13 +55,13 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
         <div className="flex items-center ml-auto gap-x-2">
           <ActionTooltip label="Edit">
             <Edit
-              onClick={() => onOpen("editChannel", { server, channel })}
+              onClick={(e) => onAction(e, "editChannel")}
               className="hidden w-4 h-4 transition group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => onAction(e, "deleteChannel")}
               className="hidden w-4 h-4 transition group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300"
             />
           </ActionTooltip>
