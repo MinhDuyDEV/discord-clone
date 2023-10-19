d278a29a06ad8098084ef0b73a6023ba59b6ec09
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-10-19 22:07:46 +0700

    Conversations Setup

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,42 +1,66 @@
-import ChatHeader from "@/components/chat/chat-header";
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import React from "react";
 
-interface ChannelIdPageProps {
-  params: { serverId: string; channelId: string };
+import { db } from "@/lib/db";
+import { getOrCreateConversation } from "@/lib/conversation";
+import { currentProfile } from "@/lib/current-profile";
+import ChatHeader from "@/components/chat/chat-header";
+
+interface MemberIdPageProps {
+  params: {
+    memberId: string;
+    serverId: string;
+  };
+  searchParams: {
+    video?: boolean;
+  };
 }
 
-const page = async ({ params }: ChannelIdPageProps) => {
+const MemberIdPage = async ({ params, searchParams }: MemberIdPageProps) => {
   const profile = await currentProfile();
+
   if (!profile) {
     return redirectToSignIn();
   }
-  const channel = await db.channel.findUnique({
-    where: {
-      id: params.channelId,
-    },
-  });
-  const member = await db.member.findFirst({
+
+  const currentMember = await db.member.findFirst({
     where: {
       serverId: params.serverId,
       profileId: profile.id,
     },
+    include: {
+      profile: true,
+    },
   });
-  if (!channel || !member) {
-    redirect("/");
+
+  if (!currentMember) {
+    return redirect("/");
   }
+
+  const conversation = await getOrCreateConversation(
+    currentMember.id,
+    params.memberId
+  );
+
+  if (!conversation) {
+    return redirect(`/servers/${params.serverId}`);
+  }
+
+  const { memberOne, memberTwo } = conversation;
+
+  const otherMember =
+    memberOne.profileId === profile.id ? memberTwo : memberOne;
+
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader
-        name={channel.name}
-        serverId={channel.serverId}
-        type="channel"
+        imageUrl={otherMember.profile.imageUrl}
+        name={otherMember.profile.name}
+        serverId={params.serverId}
+        type="conversation"
       />
     </div>
   );
 };
 
-export default page;
+export default MemberIdPage;
